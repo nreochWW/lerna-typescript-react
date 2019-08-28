5a65ec8e171dfc4c75afd8aa3c7cb0fc75778e38
Nigel.Reoch <nigel.reoch@ww.com>
2019-08-28 11:04:48 -0400

    change

--- packages/Hello/lib/index.js
+++ packages/Hello/lib/index.js
@@ -2,11 +2,12 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
 const world_typescript_react_1 = require("world-typescript-react");
-const Hello = (props) => (React.createElement("h1", null,
-    "Hello World from ",
-    props.compiler,
-    " and ",
-    props.framework,
-    "!",
+const Hello = (props) => (React.createElement("div", { style: { border: "1px solid blue" } },
+    React.createElement("h1", null,
+        "Hello World from ",
+        props.compiler,
+        " and ",
+        props.framework,
+        "!"),
     React.createElement(world_typescript_react_1.default, { compiler: "TypeScript", framework: "React" })));
 exports.default = Hello;
--- packages/World/lib/index.js
+++ packages/World/lib/index.js
@@ -1,10 +1,11 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 const React = require("react");
-const World = (props) => (React.createElement("h1", null,
-    "Hello World from ",
-    props.compiler,
-    " and ",
-    props.framework,
-    "!"));
+const World = (props) => (React.createElement("div", { style: { border: "1px solid blue" } },
+    React.createElement("h1", null,
+        "Hello World from ",
+        props.compiler,
+        " and ",
+        props.framework,
+        "!")));
 exports.default = World;
